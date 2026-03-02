79aded0c74f45f34305dda01ffe0efb8d3771c08
Heborine <36609533+Heborine@users.noreply.github.com>
2026-03-02 11:46:23 -0800

    7-segment base code

--- game.v
+++ game.v
@@ -10,12 +10,14 @@ module Top(
     reg reset_debounce;
     reg reset_prev;
     reg [16:0] debounce_count_reset;
+    reg [19:0] refresh_counter;
 
     function automatic void [6:0] increment_score (input [6:0] score, input [6:0] point_inc);
         score = score + point_inc;
     endfunction
 
     always @(posedge clk) begin
+        refresh_counter <= refresh_counter + 1;
         reset_sync1 <= rst;
         reset_sync2 <= reset_sync1;
     end
@@ -35,6 +37,51 @@ module Top(
     reset_prev <= reset_debounce;
 
     if (reset_debounce && !reset_prev) begin
+        refresh_counter <= 0;
     end
+
+    assign LED_activating_counter = refresh_counter[19:18];
+
+    always @(*) begin
+        case(LED_activating_counter)
+            2'b00: begin
+                an <= 4'b0111;
+                LED_DISP <= mins/10;
+            end
+            2'b01: begin
+                LED_DISP <= mins%10;
+            end
+            2'b10: begin
+                an <= 4'b1101;
+                LED_DISP <= secs/10;
+            end
+            2'b11: begin
+                an <= 4'b1110;
+                LED_DISP <= secs%10;
+            end
+            default: begin
+                LED_DISP <= mins/10;
+            end
+        endcase
+    end
+
+
+    always @(*) begin
+        case(LED_DISP)
+            4'b0000: seg <= 7'b1000000; // "0"    
+            4'b0001: seg <= 7'b1111001; // "1"
+            4'b0010: seg <= 7'b0100100; // "2"
+            4'b0011: seg <= 7'b0110000; // "3"
+            4'b0100: seg <= 7'b0011001; // "4"
+            4'b0101: seg <= 7'b0010010; // "5"
+            4'b0110: seg <= 7'b0000010; // "6"
+            4'b0111: seg <= 7'b1111000; // "7"
+            4'b1000: seg <= 7'b0000000; // "8"    
+            4'b1001: seg <= 7'b0010000; // "9"
+            4'b1111: seg <= 7'b1111111; // "" (blink/blank)
+            default: seg <= 7'b1000000; // "0"
+        endcase
+    end
+
 
 endmodule

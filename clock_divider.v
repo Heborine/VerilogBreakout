5f103d9054b78ee8e4db8150cce539ab05e02ffc
Heborine <36609533+Heborine@users.noreply.github.com>
2026-03-04 11:06:03 -0800

    debug clock divider

--- clock_divider.v
+++ clock_divider.v
@@ -1,18 +1,19 @@
 module clock_divider (
-    input clk, // 100 Mhz
-    input rst,
-    output dclk // 25Mhz
-)
+    input wire clk, // 100 Mhz
+    input wire rst,
+    output reg dclk // 25Mhz
+);
+
     reg [1:0] counter_25Mhz = 2'd0;
 
-    always(@posedge clk or posedge rst) begin
+    always @(posedge clk or posedge rst) begin
         if (rst) begin
             counter_25Mhz <= 2'd0;
-            clk_25Mhz = 1'b0;
+            dclk <= 1'b0;
         end else begin
             if(counter_25Mhz == 1'd1) begin
                 counter_25Mhz <= 2'd0;
-                clk_25Mhz <= ~clk_25Mhz;
+                dclk <= ~dclk;
             end
             else begin
                 counter_25Mhz <= counter_25Mhz + 1'b1;

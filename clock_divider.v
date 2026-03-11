02c84b7c454ce258aeb6d2e50416d4eab46b62a4
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2026-03-11 09:18:26 -0700

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -6,19 +6,14 @@ module clock_divider (
 
     reg [1:0] counter_25Mhz = 2'd0;
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
+    always @(posedge clk) begin
+        if(counter_25Mhz == 1'd1) begin
             counter_25Mhz <= 2'd0;
-            dclk <= 1'b0;
-        end else begin
-            if(counter_25Mhz == 1'd1) begin
-                counter_25Mhz <= 2'd0;
-                dclk <= ~dclk;
-            end
-            else begin
-                counter_25Mhz <= counter_25Mhz + 1'b1;
-            end  
-        end   
+            dclk <= ~dclk;
+        end
+        else begin
+            counter_25Mhz <= counter_25Mhz + 1'b1;
+        end
     end
 
 endmodule
